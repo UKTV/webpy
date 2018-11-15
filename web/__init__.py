2aaf5b71fe1d100515c9fa690b789681ae11e459
PhysTom88 <tom.ravenscroft@uktv.co.uk>
2018-11-15 12:08:31 +0000

    changed version

--- web/__init__.py
+++ web/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import generators
 
-__version__ = "0.39"
+__version__ = "0.39.1-hotfix"
 __author__ = [
     "Aaron Swartz <me@aaronsw.com>",
     "Anand Chitipothu <anandology@gmail.com>"
